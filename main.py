861fcc70d721625234f05a0e175ce88b22eb6f35
jean-guiollot <jean.guiollot@edu.esiee.fr>
2024-09-23 11:18:13 +0000

    10/10

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+"""
+Module pour construire une liste de tuple qui se base sur une chaine de caractere.
+"""
 #### Imports et définition des variables globales
+import sys
 
+# Augmente la limite de récursion
+sys.setrecursionlimit(1500)  # Ajuste cette valeur selon tes besoins
 
 #### Fonctions secondaires
 
 
+#### Fonction itérative
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères 
+    passée en argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -13,35 +21,55 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
+    if not s:
+        return []
 
-    return [ ]
+    caractere = [s[0]]  # Liste des caractères rencontrés
+    occurence = [1]     # Liste des occurrences
+
+    for k in range(1, len(s)):
+        if s[k] == s[k - 1]:
+            occurence[-1] += 1  # Augmenter le nombre d'occurrences du dernier caractère
+        else:
+            caractere.append(s[k])  # Ajouter un nouveau caractère
+            occurence.append(1)     # Ajouter 1 pour sa première occurrence
+
+    return list(zip(caractere, occurence))  # Transformer en tuples
 
 
-def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+#### Fonction récursive
+def artcode_r(s, index=0):
+    """Retourne la liste de tuples encodant une chaîne de caractères 
+    passée en argument selon un algorithme récursif.
 
     Args:
         s (str): la chaîne de caractères à encoder
 
     Returns:
-        list: la liste des tuples (caractère, nombre d'occurences)
+        list: la liste des tuples (caractère, nombre d'occurrences)
     """
-    
-    # votre code ici
+    # Cas de base : si on a atteint la fin de la chaîne
+    if index >= len(s):
+        return []
 
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
+    current_char = s[index]
+    count = 1
+    # Compte les occurrences du caractère actuel
+    while index + 1 < len(s) and s[index + 1] == current_char:
+        count += 1
+        index += 1
 
-    return []
-    
+    # Appelle récursivement pour le reste de la chaîne
+    return [(current_char, count)] + artcode_r(s, index + 1)
+
 
 #### Fonction principale
 
 
 def main():
+    """
+    Fonction principale.
+    """
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
